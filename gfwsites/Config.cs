c8e04360406ceb8b8fd75bd183b18272d86fb802
sy1989 <sy1989@users.noreply.github.com>
2023-12-23 12:41:26 +0800

    up

---
+++ gfwsites/Config.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace gfwsites
+{
+    internal class Configs
+    {
+        public static Configs LoadConfiguration(string filename)
+        {
+            TextReader reader = new StreamReader(filename);
+            string json = reader.ReadToEnd();
+            reader.Close();
+            Configs config = JsonConvert.DeserializeObject<Configs>(json);
+            return config;
+        }
+        public Dictionary<string, List<string>> Config { get; set; }
+    }
+}
--- gfwsites/Program.cs
+++ gfwsites/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace gfwsites
+﻿using System.Text;
+
+namespace gfwsites
 {
     internal class Program
     {
@@ -6,8 +8,81 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine(System.Environment.CurrentDirectory);
-            Console.WriteLine(args[0]);
+            var config_file = args[0];
+            var config = Configs.LoadConfiguration(config_file);
+            var datapath = args[1];
+            var savepath = Directory.CreateDirectory("save");
+            foreach(var a in config.Config)
+            {
+                var filename =Path.Combine(savepath.FullName, a.Key);
+                var c  = new StringBuilder();
+                foreach (var b in a.Value)
+                {
+                    var datafile = Path.Combine(datapath, b);
+                    var sr = GetData(datafile);
+                    if(!string.IsNullOrEmpty(sr) )
+                    {
+                        c.AppendLine(sr);
+                    }
+                }
+                File.WriteAllText(filename, c.ToString());
+            }
+        }
+        static string GetData(string filename)
+        {
+            var a = File.ReadAllLines(filename);
+            var sb = new StringBuilder();
+            foreach (var b in a)
+            {
+                var c = GetData(b);
+                if (!string.IsNullOrEmpty(c))
+                {
+                    sb.AppendLine(c);
+                }
+            }
+            return sb.ToString();
+        }
+        static string GetDomain(string line)
+        {
+            if (string.IsNullOrEmpty(line))
+                return null;
+            var a = line.Trim();
+            if (string.IsNullOrEmpty(a))
+                return null;
+            else if (a.StartsWith("#"))
+                return null;
+            else if (a.Contains("."))
+            {
+                if (a.StartsWith("regexp:")|| a.StartsWith("regexp:"))
+                {
+                    return null;
+                }
+                if (a.StartsWith("full:"))
+                {
+                    a = a.Remove(0, 5);
+                }
+                if (a.Contains("@"))
+                {
+                    var b = a.Split('@');
+                    if (b[1] == "cn")
+                        return null;
+                    a = b[0].Trim();
+                }
+                if (a.Contains("#"))
+                {
+                    var b = a.Split('#');
+                    a = b[0].Trim();
+                }
+                return a;
+            }
+            else
+            {
+                return null;
+            }
+
+
 
         }
+
     }
 }
